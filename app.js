ca0db4f53dce52e43f71011a2f0bff56494db39a
shahbaz ali <shahbaz_ali@outlook.in>
2021-11-02 16:16:47 +0530

    add GET

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 
-app = express()
+app = express();
 
-app.get("/", (req, res)=>{
-    res.send("working");
-})
+app.set("view-engine", "ejs");
 
-app.listen(8000, ()=>{
-    console.log("Server started on port 8000");
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", (req, res) => {
+  Article.find(function(err, results){
+    if(!err){
+        res.send(results);
+    } else{
+        console.log(err);
+    }
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
