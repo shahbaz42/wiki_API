3f483dee5f299c2baa3b51e250318e145eb91603
shahbaz ali <shahbaz_ali@outlook.in>
2021-11-03 13:51:40 +0530

    handle request targetting specific article

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { userInfo } = require("os");
 
 app = express();
 
@@ -12,45 +13,77 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 const articleSchema = {
-    title: String,
-    content: String
+  title: String,
+  content: String,
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
+// Requests targetting all articles.
 
-
-app.get("/articles", (req, res) => {
-  Article.find(function(err, results){
-    if(!err){
+app
+  .get("/articles", (req, res) => {
+    Article.find(function (err, results) {
+      if (!err) {
         res.send(results);
-    } else{
+      } else {
         console.log(err);
-    }
-  });
-})
-.post("/articles", (req, res)=>{
-    const article = new Article({
-        title : req.body.title,
-        content: req.body.content 
-    })
-    article.save((err, success)=>{
-        if(! err){
-            res.send("Successfully added a new article");
-        } else {
-            res.send(err);
-        }
-    })
-})
-.delete("/articles", (req, res)=>{
-    Article.deleteMany({}, function(err){
-        if(! err){
-            res.send("Successfully deleted all articles");
-        } else{
-            res.send(err);
-        }
+      }
     });
-});
+  })
+  .post("/articles", (req, res) => {
+    const article = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    article.save((err, success) => {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete("/articles", (req, res) => {
+    Article.deleteMany({}, function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+// Requests targetting a specific article
+
+app
+  .route("/articles/:articleTitle")
+
+  .get(function (req, res) {
+    Article.findOne({ title: req.params.articleTitle }, (err, found) => {
+      if (found) {
+        res.send(found);
+      } else {
+        res.send("No article found.");
+      }
+    });
+  })
+  .put(function (req, res) {
+    Article.updateOne(
+      { title: req.params.articleTitle },
+      {
+        title: req.body.title,
+        content: req.body.content
+      },
+      (err, result) => {
+        if (!err) {
+          res.send("Successfully updated");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
 
 app.listen(8000, () => {
   console.log("Server started on port 8000");
